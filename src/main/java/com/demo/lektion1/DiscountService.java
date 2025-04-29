226abc49e7e951b5de8f3ebe82f01987f70cfc34
Amanda Grip <amanda.grip@stud.sti.se>
2025-04-29 16:32:39 +0200

    first commit

---
+++ src/main/java/com/demo/lektion1/DiscountService.java
@@ -0,0 +1,13 @@
+package com.demo.lektion1;
+
+public class DiscountService {
+
+    public double sum;
+    public double discount;
+
+    public int applyDiscount() {
+
+        return double sum;
+
+    }
+}
---
+++ src/main/java/com/demo/lektion1/Main.java
@@ -0,0 +1,8 @@
+package com.demo.lektion1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
